c5f4ed1a8be04613e09e739297560e529a613437
Peter Horton <phorton@softstackfactory.com>
2020-10-05 15:26:50 -0700

    (feat:zsh)  jj esc is working

---
+++ .zsh-plugins.zsh
@@ -0,0 +1,48 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+VIM_MODE_VICMD_KEY='^D'
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ZSH_THEME="robbyrussell"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+  git
+  z
+  dircycle
+# colorize
+  ssh-agent
+  vi-mode
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  )
+bindkey -s jj '\e'
+bindkey jj vi-cmd-mode
+source $ZSH/oh-my-zsh.sh
+zstyle ':completion:*' menu select
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
